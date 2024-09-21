4a94864832b0b1b0497c9ea66a6bf88c6e7006bf
MOHAMEDAYMAN1313 <modyayman230@gmail.com>
2024-09-21 14:43:47 +0300

    v1.0.0

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define hamada 60
+
+int main()
+{
+int iter;
+int x = hamada;
+int y = 15 , z = 30;
+printf("x value : %i\n",x);
+printf("y value : %i\n",y);
+printf("z value : %i\n",z);
+for(iter = 0; iter < 10; iter++)
+{
+printf("amit nasr 85\n");
+}
+return 0;
+}
